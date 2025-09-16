a06504548dfbfe07617194275860d307719d6db1
keriyason <keriyason@lcad.edu>
2025-09-15 19:43:48 -0700

    added button
    feat scene changer button for Start Game

---
+++ Assingment 2/Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement; // Important: Add this namespace
+
+public class SceneChanger : MonoBehaviour
+{
+    // Function to load a scene by its name
+    public void LoadSceneByName(string SampleScene)
+    {
+        SceneManager.LoadScene(SampleScene);
+    }
+
+    // Function to load a scene by its build index
+    public void LoadSceneByIndex (int sceneIndex)
+    {
+        SceneManager.LoadScene(0);
+    }
+}
